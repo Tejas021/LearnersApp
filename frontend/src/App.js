e359d36fb41aee133439e2c23c33e29172c38582
unknown <akshayhegde158@gmail.com>
2021-12-11 21:38:15 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 import Home from './pages/Home/Home';
 import TeacherSignIn from './pages/Signin/TeacherSignIn';
 import StudentSignIn from './pages/Signin/StudentSignIn';
+import Navbar from './pages/Navbar/Navbar';
 
 
 function App() {
@@ -11,9 +12,11 @@ function App() {
     <div className="App">
      <Router>
        <Routes>
+         
          <Route exact path="/" element={<Home/>}/>
          <Route path="teacher-signin" element={<TeacherSignIn/>}/>
          <Route path="student-signin" element={<StudentSignIn/>}/>
+         <Route path="navbar" element={<Navbar/>}/>
        </Routes>
      </Router>
     </div>
